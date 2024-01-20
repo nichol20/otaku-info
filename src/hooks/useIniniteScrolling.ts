67af38cdddba0cd8b72a0dd79d6352801d10fb03
Nicholas <nick.nogue28@outlook.com>
2024-01-19 22:26:48 -0300

    fix useInfiniteScrolling calculation

--- src/hooks/useIniniteScrolling.ts
+++ src/hooks/useIniniteScrolling.ts
@@ -1,34 +1,38 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 type UseInfiniteScrolling = (
   onReachBottom: () => (() => void) | void,
   dependencies?: ReadonlyArray<unknown>
-) => void
+) => void;
 
-export const useInfiniteScrolling: UseInfiniteScrolling = (onReachBottom, dependencies) => {
-
+export const useInfiniteScrolling: UseInfiniteScrolling = (
+  onReachBottom,
+  dependencies
+) => {
   useEffect(() => {
-    let cleanupFunction = () => {}
+    let cleanupFunction = () => {};
 
     const handleScroll = () => {
       // The scrollTop property sets or returns the number of pixels an element's content is scrolled vertically
-      const scrollTop = document.documentElement.scrollTop 
+      const scrollTop = Math.ceil(document.documentElement.scrollTop);
       // interior height of the window in pixels
-      const windowHeight = window.innerHeight
+      const windowHeight = window.innerHeight;
       // The scrollHeight returns the height of an element
-      const scrollHeight = document.documentElement.scrollHeight
-    
-      if (scrollTop + windowHeight >= scrollHeight) {
-        const cb = onReachBottom()
-        cleanupFunction = cb ? cb : () => {}
-      }
-    }
+      const scrollHeight = document.documentElement.scrollHeight;
 
-    window.addEventListener("scroll", handleScroll)
+      console.log({ scrollTop, windowHeight, scrollHeight });
+
+      if (scrollTop + windowHeight >= scrollHeight) {
+        const cb = onReachBottom();
+        cleanupFunction = cb ? cb : () => {};
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll)
-      cleanupFunction()
-    }
-  }, dependencies)
-}
+      window.removeEventListener("scroll", handleScroll);
+      cleanupFunction();
+    };
+  }, dependencies);
+};
